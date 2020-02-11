5def9f12272e51ee387a808b20af6c2704f64582
Nathaniel Makinde <nathmankind@gmail.com>
2020-02-11 01:37:56 +0100

    Searching & Filtering feature implemented

--- project_api/views.py
+++ project_api/views.py
@@ -14,6 +14,7 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly, AllowAny
 from rest_framework.status import (HTTP_200_OK, HTTP_400_BAD_REQUEST,
                                    HTTP_404_NOT_FOUND)
 from rest_framework.response import Response
+from rest_framework import filters
 
 
 # Create your views here.
@@ -56,6 +57,8 @@ class ProjectList( APIView):
 
 #Retrieve all and create projects
 class ProjectViewset(viewsets.ModelViewSet):
+    search_fields = ['name']
+    filter_backends = (filters.SearchFilter,)
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
 
